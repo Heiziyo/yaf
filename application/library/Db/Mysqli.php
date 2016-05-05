2020732dd9f74dfa6acc001af7058762b9214f5f
Heiziyo <960553031@qq.com>
2016-05-05 18:08:22 +0800

    添加mysqli

---
+++ application/library/Db/Mysqli.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/5/5
+ * Time: 18:03
+ */
+class Mysqli
+{
+
+    public  function __construct()
+    {
+    }
+
+    public function connect(){
+
+
+    }
+}
